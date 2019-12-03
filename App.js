b9839753f23e792d361a99735ab57bddd34fe068
mmessmer <mmessmer@unibz.it>
2019-12-03 15:41:35 +0100

    new fetch method

--- App.js
+++ App.js
@@ -3,50 +3,55 @@ import { Provider } from 'unstated';
 import { Text, ListView } from 'react-native';
 
 const styles = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center',
+	fontFamily: 'sans-serif',
+	textAlign: 'center',
 };
 
 export default class App extends Component {
-  state = {
-    loading: true,
-    data: [],
-  };
-  getPOIsFromApiAsync(lat, lon) {
-    return fetch(
-      'https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gscoord=' +
-        lat +
-        '|' +
-        lon +
-        '&gsradius=10000&gslimit=10&format=json'
-    )
-      .then(response => response.json())
-      .then(responseJson => {
-        this.setState({ loading: false, data: responseJson.query.geosearch });
-        console.log(this.state.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
+	state = {
+		loading: true,
+		data: [],
+	};
 
-  componentDidMount() {
-    if (this.state.loading) {
-      this.getPOIsFromApiAsync(45, 7);
-    }
-  }
+	getPOIsFromApiAsync(lat, lon) {
+		let url = "https://en.wikipedia.org/w/api.php"; 
 
-  render() {
-    if (this.state.loading) {
-      return <Text>hey</Text>;
-    }
-    return (
-      <Provider>
-        <ListView
-          dataSource={this.state.data}
-          renderRow={rowData => <Text>{rowData.title}</Text>}
-        />
-      </Provider>
-    );
-  }
+		let params = {
+			action: "query",
+			list: "geosearch",
+			gscoord: lat + "|" + lon,
+			gsradius: "10000",
+			gslimit: "10",
+			format: "json"
+		};
+
+		url = url + "?origin=*";
+		Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+		fetch(url)
+			.then(function(response){return response.json();})
+			.then(function(response) {
+				let pages = response.query.geosearch;
+				for (var place in pages) {
+					console.log(pages[place].title);
+				}
+			})
+			.catch(function(error){console.log(error);});
+	}
+
+	render() {
+		this.getPOIsFromApiAsync(45,7);
+		
+		if (this.state.loading) {
+			return <Text>hey</Text>;
+		}
+		return (
+			<Provider>
+				<ListView
+					dataSource={this.state.data}
+					renderRow={rowData => <Text>{rowData.title}</Text>}
+				/>
+			</Provider>
+		);
+	}
 }
