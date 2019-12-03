1469231cdfdabb7bf36b711e855e1b5d2d3923d6
mmessmer <mmessmer@unibz.it>
2019-12-03 14:38:32 +0100

    added getPOIsFromApi function

--- App.js
+++ App.js
@@ -1,19 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Provider } from 'unstated';
+import { Text, ListView } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+};
+
+export default class App extends Component {
+  state = {
+    loading: true,
+    data: [],
+  };
+  getPOIsFromApiAsync(lat, lon) {
+    return fetch(
+      'https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gscoord=' +
+        lat +
+        '|' +
+        lon +
+        '&gsradius=10000&gslimit=10&format=json'
+    )
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({ loading: false, data: responseJson.query.geosearch });
+        console.log(this.state.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  componentDidMount() {
+    if (this.state.loading) {
+      this.getPOIsFromApiAsync(45, 7);
+    }
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <Text>hey</Text>;
+    }
+    return (
+      <Provider>
+        <ListView
+          dataSource={this.state.data}
+          renderRow={rowData => <Text>{rowData.title}</Text>}
+        />
+      </Provider>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
