cffc91d6cff692c64a822cbfe0a14658fbde1d8e
Neli Mujic <neli.mujic@hotmail.it>
2019-12-05 00:25:52 +0100

    geolocation added

--- Components/ScreenA.js
+++ Components/ScreenA.js
@@ -15,29 +15,105 @@ function Item({ title }) {
 
 export default class ScreenA extends Component {
 
-	render() {
-		//this.getPOIsFromApiAsync(45, 7);
-		return (
-				<SafeAreaView style={styles.container}>
-					<Subscribe to={[POIsContainer]}>
-						{
-							pois => {
-								const { poisList, error, loading } = pois.state;
-								
-								if (!poisList && !error && !loading) {
-									pois.getPOIsFromApiAsync(45,7);
-								}
-								return (<FlatList
-											data={poisList}
-											renderItem={({ item }) => <Item title={item.title} />}
-											keyExtractor={item => item.pageid}
-										/>);
-							}
-						}
-					</Subscribe>
-				</SafeAreaView>
-			);
-	}
+  constructor() {
+    super();
+    this.state = {
+      ready: false,
+      where: { lat: null, lon: null },
+      error: null,
+
+    }
+
+  }
+
+  componentDidMount() {
+
+    let geoOptions = {
+      enableHighAccurancy: true,
+      timeOut: 20000,
+      maximumAge: 60 * 60 * 24,
+    };
+
+    this.setState({ ready: false, error: null }),
+      navigator.geolocation.getCurrentPosition(this.geoSuccess, this.geoFailure, geoOptions)
+  }
+
+
+  geoSuccess = (position) => {
+
+
+    var numLat = position.coords.latitude;
+    var numLon = position.coords.longitude;
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+
+    numLat = numLat.toFixed(0);
+    numLon = numLon.toFixed(0);
+
+    console.log(numLat);
+    console.log(numLon);
+
+    this.setState({
+      ready: true,
+      where: { lat: position.coords.latitude, lon: position.coords.longitude }
+    })
+
+  }
+
+  geoFailure = (err) => {
+    this.setState({ error: err.message });
+  }
+
+
+
+  render() {
+
+    //this.getPOIsFromApiAsync(45, 7);
+    return (
+      <SafeAreaView style={styles.container}>
+
+        <View>
+          {!this.state.ready && (
+            <Text> Using Geolocation</Text>
+          )}
+          {this.state.error && (
+            <Text>{this.state.error}</Text>
+          )}
+          {this.state.ready && (
+            <Text>
+              Latitude: {this.state.where.lat}
+              Longitude: {this.state.where.lon}
+            </Text>
+
+          )}
+
+        </View>
+
+
+        <Subscribe to={[POIsContainer]}>
+
+          {
+            pois => {
+              const { poisList, error, loading } = pois.state;
+
+              if (!poisList && !error && !loading) {
+                pois.getPOIsFromApiAsync(46, 11);
+              }
+              return (
+                <FlatList
+                  data={poisList}
+                  renderItem={({ item }) => <Item title={item.title} />}
+                  keyExtractor={item => item.pageid}
+                />
+
+              );
+
+            }
+          }
+        </Subscribe>
+      </SafeAreaView>
+    );
+  }
 }
 
 
@@ -46,10 +122,11 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   item: {
-    backgroundColor: '#f9c2ff',
+    backgroundColor: '#4463a0',
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
+    borderRadius: 10,
   },
   title: {
     fontSize: 32,
