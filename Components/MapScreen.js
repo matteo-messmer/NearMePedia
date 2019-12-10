5fcc74f7cc45a3279f48958451314d87be3aa974
mmessmer <mmessmer@unibz.it>
2019-12-10 15:40:43 +0100

    modified map screen

--- Components/MapScreen.js
+++ Components/MapScreen.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-export default
-class App extends React.Component {
+import { Marker } from 'react-native-maps';
+
+export default class App extends React.Component {
   render() {
     return (
       <MapView
-         style={{ flex: 1 }}
-         provider={PROVIDER_GOOGLE}
-         showsUserLocation
-         initialRegion={{
-         latitude: 37.78825,
-         longitude: -122.4324,
-         latitudeDelta: 0.0922,
-         longitudeDelta: 0.0421}}
-      />
+        style={{ flex: 1 }}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}>
+        <Marker
+          coordinate={{ latitude: 28.57966, longitude: 77.32111 }}
+          title={'JavaTpoint'}
+          description={'Java Training Institute'}
+        />
+      </MapView>
     );
   }
 }
